1e67cb74eb76da6120504d3381d922bfe6d8cb29
pluveto <i@pluvet.com>
2023-11-08 10:37:00 +0800

    feat: oneline reviewer

--- x/reviewer/oneline/lib.go
+++ x/reviewer/oneline/lib.go
@@ -3,46 +3,13 @@ package oneline
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/pluveto/ankiterm/x/automata"
+	"github.com/pluveto/ankiterm/x/reviewer"
 	"github.com/pluveto/ankiterm/x/xmisc"
-	"github.com/pluveto/ankiterm/x/xslices"
 )
-
-const (
-	ActionAnswer = "answer"
-	ActionSkip   = "skip"
-	ActionAbort  = "abort"
-)
-
-type Action interface {
-	getCode() string
-}
-
-type AnswerAction struct {
-	CardEase int
-}
-
-func (a AnswerAction) getCode() string {
-	return ActionAnswer
-}
-
-type SkipAction struct {
-}
-
-func (a SkipAction) getCode() string {
-	return ActionSkip
-}
-
-type AbortAction struct {
-}
-
-func (a AbortAction) getCode() string {
-	return ActionAbort
-}
 
 func Execute(am *automata.Automata, deck string) {
 	if am == nil {
@@ -67,42 +34,30 @@ func Execute(am *automata.Automata, deck string) {
 			}
 			panic(err)
 		}
-
-		p := tea.NewProgram(initialModel(card))
-		if _, err := p.Run(); err != nil {
+		m, _ := tea.NewProgram(initialModel(card)).Run()
+		if err != nil {
 			fmt.Printf("Alas, there's been an error: %v", err)
 			os.Exit(1)
 		}
-	}
-}
+		out := m.(model)
 
-func awaitEnter() {
-	fmt.Scanln()
-}
-
-func awaitAction(validRange []int) Action {
-	print("awaitAction")
-	var input string
-	fmt.Scanln(&input)
-	// try parse int
-	i, err := strconv.Atoi(input)
-	if err == nil {
-		if xslices.Contains(validRange, i) {
-			return AnswerAction{CardEase: i}
-		} else {
-			fmt.Printf("invalid input \"%s\" out of range, try again: \n", input)
-			return awaitAction(validRange)
+		action := out.action
+		if action == nil {
+			// quit
+			fmt.Println("Bye!")
+			return
 		}
-	}
 
-	switch input {
-	case "s":
-		return SkipAction{}
-	case "a":
-		return AbortAction{}
-	default:
-		fmt.Printf("invalid input \"%s\", try again: \n", input)
-		return awaitAction(validRange)
+		switch code := action.GetCode(); code {
+		case reviewer.ActionAbort:
+			return
+		case reviewer.ActionSkip:
+			continue
+		case reviewer.ActionAnswer:
+			am.AnswerCard(action.(reviewer.AnswerAction).CardEase)
+		default:
+			panic("unknown action code " + code)
+		}
 	}
 }
 
--- x/reviewer/oneline/state.go
+++ x/reviewer/oneline/state.go
@@ -2,19 +2,24 @@ package oneline
 
 import (
 	"fmt"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/fatih/color"
 	"github.com/pluveto/ankiterm/x/ankicc"
+	"github.com/pluveto/ankiterm/x/reviewer"
 )
 
 // model
-// Q: <question> A: <answer> 1. <choice1> 2. <choice2> 3. <choice3> 4. <choice4>
+// [R] <content> 1. <choice1> 2. <choice2> 3. <choice3> 4. <choice4>
 // /                                            ^cursor     |||||selected
 type model struct {
-	question string
-	answer   string
-	choices  []string
-	cursor   int
+	question    string
+	answer      string
+	answerShown bool
+	choices     []string
+	chosen      int
+	action      reviewer.Action
 }
 
 func initialModel(card *ankicc.CurrentCard) model {
@@ -25,26 +30,30 @@ func initialModel(card *ankicc.CurrentCard) model {
 		buttons = append(buttons, button)
 	}
 	return model{
-		question: format(card.Question),
-		answer:   format(card.Answer),
-		choices:  buttons,
-		cursor:   0,
+		question:    strings.ReplaceAll(format(card.Question), "\n", " "),
+		answer:      strings.ReplaceAll(format(card.Answer), "\n", " "),
+		choices:     buttons,
+		answerShown: false,
+		chosen:      0,
 	}
 }
 
 func (m model) View() string {
+	if !m.answerShown {
+		return fmt.Sprintf("[R] %s %s", m.question, color.HiBlueString("Show Answer"))
+	}
 	choicesStr := ""
 	for i, choice := range m.choices {
 		choiceStr := ""
-		if i == m.cursor {
-			choiceStr = fmt.Sprintf("<%s>", choice)
+		if i == m.chosen {
+			choiceStr = color.HiBlueString(choice)
 		} else {
 			choiceStr = choice
 		}
 
 		choicesStr += choiceStr + " "
 	}
-	s := fmt.Sprintf("Q: %s -- A: %s -- %s", m.question, m.answer, choicesStr)
+	s := fmt.Sprintf("[R] %s -- %s", m.answer, choicesStr)
 	return s
 }
 
@@ -54,32 +63,60 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if !m.answerShown {
+		return updateWhenAnswerHidden(m, msg)
+	}
 	switch msg := msg.(type) {
 
-	// Is it a key press?
 	case tea.KeyMsg:
 
-		// Cool, what was the actual key pressed?
 		switch msg.String() {
 
 		case "left":
-			m.cursor--
-			if m.cursor < 0 {
-				m.cursor = 0
+			m.chosen--
+			if m.chosen < 0 {
+				m.chosen = 0
 			}
 		case "right":
-			m.cursor++
-			if m.cursor >= len(m.choices) {
-				m.cursor = len(m.choices) - 1
+			m.chosen++
+			if m.chosen >= len(m.choices) {
+				m.chosen = len(m.choices) - 1
 			}
+		case "1", "2", "3", "4":
+			m.chosen = int(msg.String()[0] - '1')
+			m.action = reviewer.AnswerAction{
+				CardEase: m.chosen + 1,
+			}
+			return m, tea.Quit
 		case "enter":
-			println("enter")
-		case "ctrl+c":
+			m.action = reviewer.AnswerAction{
+				CardEase: m.chosen + 1,
+			}
+			return m, tea.Quit
+		case "q", "ctrl+c":
+			m.action = reviewer.AbortAction{}
+			return m, tea.Quit
+		case "s":
+			m.action = reviewer.SkipAction{}
 			return m, tea.Quit
 		}
 	}
 
 	// Return the updated model to the Bubble Tea runtime for processing.
 	// Note that we're not returning a command.
+	return m, nil
+}
+
+func updateWhenAnswerHidden(m model, msg tea.Msg) (tea.Model, tea.Cmd) {
+	// check what msg it is
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "enter":
+			m.answerShown = true
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	}
 	return m, nil
 }
